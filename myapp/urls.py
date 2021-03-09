bb8e795f5b931a193b19748bbfad669d8baf67f2
Wadaarik <nicolasboudier12@gmail.com>
2021-03-09 09:50:39 +0100

    links pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    path('', views.index),# le '' correspond à ce qui va suivre dans l'url (localhost/myapp)
-    path('about/', views.about),
-    path('jewellery/', views.jewellery),
-    path('contact/', views.contact),
+    path('', views.index, name='index'),# le '' correspond à ce qui va suivre dans l'url (localhost/myapp)
+    path('about/', views.about, name='about'),
+    path('jewellery/', views.jewellery, name='jewellery'),
+    path('contact/', views.contact, name='contact'),
 ]
